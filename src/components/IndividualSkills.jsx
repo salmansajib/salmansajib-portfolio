d29c3f0c23ddacaa3e8ad65769bab5a17e31191a
Salman-sajib <sfsajib@gmail.com>
2024-07-07 18:40:06 +0600

    modified some styles

--- src/components/IndividualSkills.jsx
+++ src/components/IndividualSkills.jsx
@@ -2,13 +2,8 @@
 
 function IndividualSkills({ iconImage, iconName, alt }) {
   return (
-    <div
-      style={{
-        backgroundImage: `linear-gradient(120deg, #bfdbfe 10%,#86efac 60%, #a5b4fc 90%)`,
-      }}
-      className="flex h-[9.4rem] w-[8.4rem] items-center justify-center rounded-[.5rem]"
-    >
-      <div className="flex h-[9rem] w-[8rem] flex-col items-center justify-center gap-2 rounded-[.45rem] bg-zinc-900">
+    <div className="flex h-[9.4rem] w-[8.8rem] items-center justify-center rounded-[.5rem] bg-gradient-to-tr from-blue-200 from-10% via-green-300 via-60% to-indigo-300 to-90%">
+      <div className="flex h-[9rem] w-[8.4rem] flex-col items-center justify-center gap-2 rounded-[.45rem] bg-zinc-900">
         <img className="size-[64px]" src={iconImage} alt={alt} />
         <p className="text-base">{iconName}</p>
       </div>
